8c05ec5938732edb94de370eea4b4d967114ee6e
Hitesh Maidasani <hmaidasani@gmail.com>
2014-02-09 19:15:07 -0500

    working version 1.0

--- app.js
+++ app.js
@@ -32,19 +32,61 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 
 io.sockets.on('connection', function (socket) {
+	// var arduino = require('duino'),
+ //    board = new arduino.Board();
+
+ //    board.on('connected', function(){
+ //      // board.write('HELLO WORLD');
+ //      console.log("hi");
+ //    });
+
+ //    board.on('data', function(data) {
+ //      console.log(data);
+ //    });
 	var serialport = require("serialport");
 	var SerialPort = serialport.SerialPort; // localize object constructor
+	var portName = "/dev/tty.usbmodemfa131";
 
+	// var serialPort = new SerialPort(portName, {
+ //        baudrate: 9600,
+ //        // defaults for Arduino serial communication
+ //         dataBits: 8, 
+ //         parity: 'none', 
+ //         stopBits: 1, 
+ //         flowControl: false, 
+ //         parser: serialport.parsers.readline("\n")
+ //    });
+	// var receivedData = "";
+	// serialPort.on("open", function () {
+	// 	console.log('open serial communication');
+	//     // Listens to incoming data
+	//     serialPort.on('data', function(data) { 
+	//       	receivedData += data.toString();
+	      	
+	//       		// save the data between 'B' and 'E'
+	//            console.log(">>>>>>"+receivedData);
+	//            receivedData = '';
+	        
+	//         // send the incoming data to browser with websockets.
+	//        // socketServer.emit('update', sendData);
+	//     });  
+	// });  
 	var sp = new SerialPort("/dev/tty.usbmodemfa131", {
 		baudrate: 9600,
-		parser: serialport.parsers.readline("\n")
+		// defaults for Arduino serial communication
+         dataBits: 8, 
+         parity: 'none', 
+         stopBits: 1, 
+         flowControl: true,
+		parser: serialport.parsers.readline("\r\n")
 	});
 
 	sp.on("open", function () {
 		// console.log('open');
 		sp.on('data', function(data) {
-			console.log('data ' +data);
-		  socket.emit('data', handleData(data));
+			// console.log('>>>>>' +data);
+			handleData(data);
+		  socket.emit('data', [handleData(data), data]);
 
 		  // socket.on('my other event', function (data) {
 		  //   console.log(data);
@@ -55,19 +97,33 @@ io.sockets.on('connection', function (socket) {
   
 });
 
+var patt1=/^[ad]\d{1,2}[io][=]\d+$/;
 function handleData(data) {
 	var result = {};
-	console.log(data);
+	// console.log("handle>>>>>"+data);
+	data = data.replace(" ", "");
 	if(data.indexOf('#') == 0) {
 		data = data.replace('#', '');
-		data.split(',').forEach(function(x){
-			console.log(x);
-		    var arr = x.split('=');
-		    console.log(arr);
-		    result[arr[0]] = arr[1];
-		});
+		var splitArr = data.split(',');
+		// console.log("===="+splitArr);
+		var obj = "";
+		for(var i = 0; i < splitArr.length; i++) {
+			obj = splitArr[i];
+			//console.log(obj);
+			
+			if(patt1.test(obj)){
+				var arr = obj.split('=');
+				// console.log(parseInt(arr[0].substring(1,3)));
+				if(parseInt(arr[0].substring(1,3)) >13 ){
+					// console.log("?????"+arr[0]);
+					return;
+				}
+					
+				result[arr[0]] = arr[1];
+			}
+		}
 	}
-	// console.log(result);
+	
 	return result;
 }
 
